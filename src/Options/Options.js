136b0746abd3a412cb2bcc90e05cc5ff37d727a8
Sara Green <smg105c@gmail.com>
2020-11-22 21:08:53 -0800

    Separated Customizer into CustomOptions

---
+++ src/Options/Options.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import slugify from 'slugify';
+
+class Options extends Component {
+    render() {
+        return (
+            <div />
+        )
+    }
+}
+
+export default Options;
