e8507c8b3a428a6b1af0328a7210789ca75f4872
Sara Green <smg105c@gmail.com>
2020-11-23 16:53:49 -0800

    Split off Feature Item from CustomOptions

--- src/CustomOptions/CustomOptions.js
+++ src/CustomOptions/CustomOptions.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import FeatureItem from '../FeatureItem/FeatureItem';
 import slugify from 'slugify';
 
 class CustomOptions extends Component {
@@ -9,19 +10,15 @@ class CustomOptions extends Component {
                 const itemHash = slugify(JSON.stringify(item));
                 
                 return (
-                    <div key={itemHash} className="feature__item">
-                        <input
-                        type="radio"
-                        id={itemHash}
-                        className="feature__option"
-                        name={slugify(feature)}
-                        checked={item.name === this.props.selected[feature].name}
-                        onChange={e => this.props.handleUpdate(feature, item)}
-                        />
-                        <label htmlFor={itemHash} className="feature__label">
-                            {item.name} ({this.props.USCurrencyFormat.format(item.cost)})
-                        </label>
-                    </div>
+                    <FeatureItem
+                        itemHash={itemHash}
+                        key={itemHash}
+                        item={item}
+                        feature={feature}
+                        selected={this.props.selected}
+                        USCurrencyFormat={this.props.USCurrencyFormat}
+                        handleUpdate={this.props.handleUpdate}
+                    />
                 );
             });
           
